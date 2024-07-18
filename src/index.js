e10db04a8f9aae203dcf0656ff037c7f1ef2d554
seojin <csj06060123@gmail.com>
2024-07-18 11:32:00 +0900

    [변경사항] 전체적인 디자인 제거 및 새로운 로직을 구성함. [기능] 할 일 추가 버튼을 눌렀을 때 모달창이 열리는 것. [기능] 모달창을 열고 난 후 추가 버튼을 눌렀을 때 제목 입력 밑에 있는 인풋의 값을 공백 여부를 판단하여 모달창이 닫힘의 여부 결정. [기능] 닫기 버튼 누르면 값이 있든 말든 그냥 닫힘. [추가된 것] 새로운 라이브러리 사용 recoil, recoil-persist 사용함. [추가된 파일] atoms.js에 state값에 담길 초기 값과 기본 값, 새로고침이나 페이지를 닫아도 유지되는 코드가 있음. [추가된 파일] reportWebVials.js 사실 어떤 파일인지 정확히 모르지만 새로 설치한 라이브러리 사용을 위한 파일을 만듦. [수정된 것] index.js에 recoil을 사용하기 위한 로직 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+//index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+import reportWebVitals from "./reportWebVials";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
